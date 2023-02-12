93c7e63911d103d1fbb0406cd33c7a56c3050489
Youngoct <youngcoder42@gmail>
2023-02-12 09:03:40 +0100

    format.c

---
+++ C_program/format.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the format
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int m = 40;
+	float k = 2.9765;
+	char h = 'I';
+
+	printf("value of m = %i\n", m);
+	printf("value of k = %f\n", k);
+	printf("value of h = %C\n", h);
+	return (0);
+}
