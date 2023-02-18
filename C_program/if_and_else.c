0ed52b3b2b5c8e205f10370db194fb73553bd160
Youngoct <youngcoder42@gmail>
2023-02-18 15:02:50 +0100

    if_and_else.c

---
+++ C_program/if_and_else.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - check the result
+ *
+ * Return: 0 as success
+ */
+int main(void)
+{
+	int s = 100;
+	int i = 500;
+
+	if (i > s)
+	{
+		printf("'i' is more powerful than s\n");
+	}
+	else
+	{
+		printf("'s' is more powerful than i\n");
+	}
+	return (0);
+}
